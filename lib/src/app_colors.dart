c900b30f535b29de2f17899bd6f351cf184b96da
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-03-26 08:59:22 -0400

    feat: add basic colors

---
+++ lib/src/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color blackColor = Color(0xFF0D0D0D);
+
+  static const Color white = Color(0xFFF2F2F2);
+
+  static const Color grey = Color(0xFF383840);
+
+  static const Color purplePrimary = Color(0xFFAF1AD9);
+  static const Color purpleSecondary = Color(0xFFAD1FBF);
+
+  static const Color pinkPrimary = Color(0xFFD929BB);
+}
---
+++ lib/src/ui/widgets/elevated_button_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../app_colors.dart';
+
+class ElevatedButtonWidget extends StatelessWidget {
+  final VoidCallback callback;
+  final String title;
+  final double? width;
+  final double? height;
+
+  const ElevatedButtonWidget({
+    super.key,
+    required this.callback,
+    required this.title,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: callback,
+      child: Container(
+        height: height,
+        padding: const EdgeInsets.all(15),
+        width: width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: const LinearGradient(
+            colors: [
+              AppColors.purplePrimary,
+              AppColors.pinkPrimary,
+            ],
+          ),
+        ),
+        child: Text(
+         title,
+          style: Theme.of(context).textTheme.labelLarge,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
