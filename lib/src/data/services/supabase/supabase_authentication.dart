4c1eb3dd8f9bf4e3decf882868249a66ece2479c
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-03-26 17:28:38 -0400

    chore: building supabase sign up

---
+++ lib/src/data/services/supabase/supabase_authentication.dart
@@ -0,0 +1,11 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseAuthentication {
+  final SupabaseClient client;
+
+  SupabaseAuthentication({required this.client});
+
+  Future<AuthResponse> signUp({required String email, required String password}) async {
+    return await client.auth.signUp(password: password, email: email);
+  }
+}
