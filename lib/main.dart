b2f9e7cb73b185b1aba4519a1eff97cb486b17c3
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-03-26 17:27:17 -0400

    feat: start supabase services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'src/app_widget.dart';
 
-void main() {
+void main() async {
+  const String url = String.fromEnvironment('supabaseUrl');
+  const String key = String.fromEnvironment('supabaseKey');
+
+  await Supabase.initialize(url: url, anonKey: key);
+
   runApp(const AppWidget());
 }
