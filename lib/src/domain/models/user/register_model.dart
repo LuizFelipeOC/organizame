015aa2abc6882dfe5483c90d27dc2b4d9492bd70
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-03-26 14:31:06 -0400

    feat: create model to register user

---
+++ lib/src/domain/models/user/register_model.dart
@@ -0,0 +1,13 @@
+class RegisterModel {
+  String username;
+  String email;
+  String password;
+  String confirmPassword;
+
+  RegisterModel({
+    this.username = '',
+    this.email = '',
+    this.password = '',
+    this.confirmPassword = '',
+  });
+}
