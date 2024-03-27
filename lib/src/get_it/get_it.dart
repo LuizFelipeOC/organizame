62400f7b3775c73d933016e73b510ceeb7ecbfd1
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-03-27 11:42:22 -0400

    refactor: add DI of RegisterController and Repository

--- lib/src/get_it/get_it.dart
+++ lib/src/get_it/get_it.dart
@@ -1,11 +1,15 @@
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../data/repositories/register_repository.dart';
 import '../data/services/supabase/supabase_authentication.dart';
+import '../ui/controller/register/register_controller.dart';
 
 final getIt = GetIt.instance;
 
 void registerService() {
   getIt.registerSingleton<SupabaseClient>(Supabase.instance.client);
-  getIt.registerFactory<SupabaseAuthentication>(() => SupabaseAuthentication(client: getIt.get<SupabaseClient>()));
+  getIt.registerSingleton<SupabaseAuthentication>(SupabaseAuthentication(client: getIt.get<SupabaseClient>()));
+  getIt.registerFactory<RegisterRepository>(() => RegisterRepository(authentication: getIt.get<SupabaseAuthentication>()));
+  getIt.registerFactory<RegisterController>(() => RegisterController(repository: getIt.get<RegisterRepository>()));
 }
