5bc768d6795764838578e0e991a0ca0cd93a4cc6
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-03-28 11:40:59 -0400

    feat: apply registered DI ForgotPassword

--- lib/src/get_it/get_it.dart
+++ lib/src/get_it/get_it.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../data/repositories/forgot_password.dart';
 import '../data/repositories/register_repository.dart';
 import '../data/services/supabase/supabase_authentication.dart';
+import '../ui/controller/forgot_password/forgot_password.dart';
 import '../ui/controller/register/register_controller.dart';
 
 final getIt = GetIt.instance;
@@ -12,4 +14,6 @@ void registerService() {
   getIt.registerSingleton<SupabaseAuthentication>(SupabaseAuthentication(client: getIt.get<SupabaseClient>()));
   getIt.registerFactory<RegisterRepository>(() => RegisterRepository(authentication: getIt.get<SupabaseAuthentication>()));
   getIt.registerFactory<RegisterController>(() => RegisterController(repository: getIt.get<RegisterRepository>()));
+  getIt.registerFactory<ForgotPasswordRepository>(() => ForgotPasswordRepository(authentication: getIt.get<SupabaseAuthentication>()));
+  getIt.registerFactory<ForgotPasswordController>(() => ForgotPasswordController(repository: getIt.get<ForgotPasswordRepository>()));
 }
