c22b1b88a46af26500b9b8961d63733cae63b6da
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-04-01 09:09:16 -0400

    feat: add flux of user redirect screen

--- lib/src/ui/pages/splash/splash_page.dart
+++ lib/src/ui/pages/splash/splash_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../get_it/get_it.dart';
+import '../../controller/splash/splash.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -8,6 +12,23 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  final _controller = getIt.get<SplashController>();
+
+  @override
+  void initState() {
+    _controller.checkUser();
+
+    _controller.addListener(() {
+      if (_controller.isAuthenticated == true) {
+        return context.pushReplacement('/home');
+      }
+
+      return context.pushReplacement('/login');
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
