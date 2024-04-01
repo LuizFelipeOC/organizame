0c807b60492cdbbe43e826d6f937d5c4bb8480d9
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-04-01 09:07:33 -0400

    test: add test check exist user authenticated

--- test/src/data/repositories/splash_repository_test.dart
+++ test/src/data/repositories/splash_repository_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:organizame/src/data/repositories/splash_repository.dart';
 import 'package:organizame/src/data/services/supabase/supabase_authentication.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MockSupabase extends Mock implements SupabaseAuthentication {}
 
@@ -19,7 +20,7 @@ void main() {
         repository = SplashRepository(authentication: authentication),
       });
 
-  test('Should test if exist user authenticated', () async {
+  test('Should test if not exist user authenticated', () async {
     when(() => authentication.checkUserAuthenticated()).thenAnswer((_) {
       return null;
     });
@@ -28,5 +29,20 @@ void main() {
 
     expect(sut, isA<bool>());
     expect(sut, isFalse);
+  });
+
+  test('Should test if exist user authenticated', () async {
+    when(() => authentication.checkUserAuthenticated()).thenAnswer((_) {
+      return Session(
+        accessToken: '',
+        tokenType: '',
+        user: const User(id: '', appMetadata: {}, userMetadata: {}, aud: '', createdAt: ''),
+      );
+    });
+
+    final sut = repository.verifyUser();
+
+    expect(sut, isA<bool>());
+    expect(sut, isTrue);
   });
 }
