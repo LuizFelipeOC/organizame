ec4bc59223db683eef2c2cd2b5f2871252384073
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2024-04-02 16:00:59 -0400

    feat: add list of projects like grid

--- lib/src/ui/pages/home/home_page.dart
+++ lib/src/ui/pages/home/home_page.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../app_colors.dart';
 import '../../../get_it/get_it.dart';
 import '../../controller/home/home_controller.dart';
 import '../../widgets/custom_app_bar.dart';
+import 'widgets/project_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -33,14 +35,19 @@ class _HomePageState extends State<HomePage> {
             valueListenable: _controller.projects,
             builder: (_, value, __) {
               return CustomAppBar(
-                title: 'Welcome',
-                leading: true,
-                icon: FontAwesomeIcons.userAstronaut,
-                callback: () => {},
-                suffix: value.isNotEmpty,
-                suffixIcon: FontAwesomeIcons.plus,
-                suffixCallback: () => context.push('/create-project'),
-              );
+                  title: 'Welcome',
+                  leading: true,
+                  icon: FontAwesomeIcons.userAstronaut,
+                  callback: () => {},
+                  suffix: value.isNotEmpty,
+                  suffixIcon: FontAwesomeIcons.plus,
+                  suffixCallback: () async {
+                    final result = await context.push<bool?>('/create-project');
+
+                    if (result == null) return;
+
+                    await _controller.fetch();
+                  });
             },
           ),
           ValueListenableBuilder(
@@ -77,8 +84,12 @@ class _HomePageState extends State<HomePage> {
                                 children: [
                                   TextSpan(
                                     recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
-                                        context.push('/create-project');
+                                      ..onTap = () async {
+                                        final result = await context.push<bool?>('/create-project');
+
+                                        if (result == null) return;
+
+                                        await _controller.fetch();
                                       },
                                     text: 'Create now',
                                     style: Theme.of(context).textTheme.labelLarge,
@@ -92,7 +103,29 @@ class _HomePageState extends State<HomePage> {
                     );
                   }
 
-                  return const SizedBox();
+                  return Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      alignment: Alignment.topCenter,
+                      child: RefreshIndicator(
+                        backgroundColor: AppColors.blackColor,
+                        color: AppColors.white,
+                        onRefresh: () async => await _controller.fetch(),
+                        child: GridView.builder(
+                          itemCount: projects.length,
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          itemBuilder: (_, int index) {
+                            final item = projects[index];
+
+                            return ProjectCard(item: item);
+                          },
+                        ),
+                      ),
+                    ),
+                  );
                 },
               );
             },
